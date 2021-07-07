22c6bab07ac64ac2b6e8de73ff9ada3b3c53b7a2
RuthWudu <87043497+RuthWudu@users.noreply.github.com>
2021-07-07 11:14:13 -0400

    first commit

---
+++ src/DataTypeVariables.java
@@ -0,0 +1,21 @@
+public class DataTypeVariables {
+    public static void main(String[] args) {
+        boolean flag = true;
+        byte myByte = 100; //-132 - 133
+        char myChar = 'a';
+        short index = 200;
+        int myInt = 3000000; // 3,000,000
+        float myFloat = 4.73F;
+        long longVar = 1234567890; // 1,234,567,899
+        double doubleVar = 1234567889.123456789;
+
+        System.out.println("flag:" + flag);
+        System.out.println("myByte:" + myByte);
+        System.out.println("myChar:" + myChar);
+        System.out.println("index:" + index);
+        System.out.println("myInt:" + myInt);
+        System.out.println("myFloat:" + myFloat);
+        System.out.println("longVar:" + longVar);
+        System.out.println("doubleVar:" + doubleVar);
+    }
+}
